b136758ccc604fc3079ccc588f76b38be91be9f6
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2021-03-10 08:54:49 +0100

    Up version

--- AddonPolylang.php
+++ AddonPolylang.php
@@ -19,7 +19,7 @@ final class AddonPolylang extends Module
 
     public function initialize(ParameterManager $parameters, Container $container)
     {
-        define('WOODY_ADDON_POLYLANG_VERSION', '1.0.0');
+        define('WOODY_ADDON_POLYLANG_VERSION', '1.0.3');
         define('WOODY_ADDON_POLYLANG_ROOT', __FILE__);
         define('WOODY_ADDON_POLYLANG_DIR_ROOT', dirname(WOODY_ADDON_POLYLANG_ROOT));
         define('WOODY_ADDON_POLYLANG_URL', basename(__DIR__) . '/Resources/Assets');
