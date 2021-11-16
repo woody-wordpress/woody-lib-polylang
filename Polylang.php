065f4d331451e27c2bf3cdd9a2f2759bf301afa2
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2021-11-16 14:25:11 +0100

    [vers] 1.2.0

--- Polylang.php
+++ Polylang.php
@@ -19,7 +19,7 @@ final class Polylang extends Module
 
     public function initialize(ParameterManager $parameters, Container $container)
     {
-        define('WOODY_LIB_POLYLANG_VERSION', '1.1.0');
+        define('WOODY_LIB_POLYLANG_VERSION', '1.2.0');
         define('WOODY_LIB_POLYLANG_ROOT', __FILE__);
         define('WOODY_LIB_POLYLANG_DIR_ROOT', dirname(WOODY_LIB_POLYLANG_ROOT));
         define('WOODY_LIB_POLYLANG_URL', basename(__DIR__) . '/Resources/Assets');
