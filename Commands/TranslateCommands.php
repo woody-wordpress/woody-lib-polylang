d116af63c8ef1b293477aa83964fa9cda70e42a5
Léo POIROUX <leo.poiroux@gmail.com>
2022-08-24 15:59:37 +0200

    url_to_postid with redirection search

--- Commands/TranslateCommands.php
+++ Commands/TranslateCommands.php
@@ -465,7 +465,7 @@ class TranslateCommands
         if (substr($key, -4) == 'link') {
             $value = (is_array($value)) ? maybe_unserialize(current($value)) : $value;
             if (is_array($value) && !empty($value['url'])) {
-                $url_to_postid = url_to_postid($value['url']);
+                $url_to_postid = $this->url_to_postid($value['url']);
                 $pll_post_id = (empty($url_to_postid)) ? null : pll_get_post($url_to_postid, $lang);
                 $value['url'] = (empty($pll_post_id)) ? $value['url'] : get_permalink($pll_post_id);
                 return maybe_serialize($value);
@@ -473,6 +473,25 @@ class TranslateCommands
         } else {
             return $this->pllacfAutoTranslate->translate_meta($value, $key, $lang, $tr_post_id, $post_id);
         }
+    }
+
+    private function url_to_postid($url)
+    {
+        $url_to_postid = url_to_postid($url);
+        if (empty($url_to_postid)) {
+            $parse_url = parse_url($url);
+            if (!empty($parse_url['path'])) {
+                $path = (substr($parse_url['path'], -1) == '/') ? substr($parse_url['path'], 0, -1) : $parse_url['path'];
+
+                global $wpdb;
+                $sql = "SELECT action_data FROM `{$wpdb->base_prefix}redirection_items` WHERE match_url = '" . $path . "' AND status = 'enabled' ORDER BY position ASC LIMIT 1;";
+                $results = $wpdb->get_results($sql);
+                if (!empty($results) && !empty($results[0]) && !empty($results[0]->action_data)) {
+                    return url_to_postid($results[0]->action_data);
+                }
+            }
+        }
+        return $url_to_postid;
     }
 
     /**
