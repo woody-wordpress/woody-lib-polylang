119cb35668d936021fb396ce95acc218b82a5d9d
Orphée <orphee.besson@raccourci.fr>
2024-07-11 11:41:09 +0200

    [vers] 2.20.2

--- Polylang.php
+++ Polylang.php
@@ -23,7 +23,7 @@ final class Polylang extends Module
 
     public function initialize(ParameterManager $parameterManager, Container $container)
     {
-        define('WOODY_LIB_POLYLANG_VERSION', '2.20.1');
+        define('WOODY_LIB_POLYLANG_VERSION', '2.20.2');
         define('WOODY_LIB_POLYLANG_ROOT', __FILE__);
         define('WOODY_LIB_POLYLANG_DIR_ROOT', dirname(WOODY_LIB_POLYLANG_ROOT));
         define('WOODY_LIB_POLYLANG_URL', basename(__DIR__) . '/Resources/Assets');
